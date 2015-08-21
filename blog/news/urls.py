7b9a3e5c90d3bc9b075562fe18f55cdbd8dcb95e
pawelkowalik <kowalik.paw@gmail.com>
2015-08-21 22:31:51 +0200

    Jednak musi tak zostać. Przez pycharma lokalnie się wykrzacza bez tego, a po zmianach na pythonanywhere się wykrzacza również.

--- blog/news/urls.py
+++ blog/news/urls.py
@@ -6,8 +6,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/?$', NewsList.as_view(), name='news-list'),
+    url(r'^(?P<page>[0-9]+)/$', NewsList.as_view(), name='news-list'),
     url(r'^news/(?P<slug>[\w\-_]+)/$', NewsDetailView.as_view(),
         name='news-detail'),
-    url(r'^(?P<page>[0-9]+)/$', NewsList.as_view(), name='news-list'),
 
 )
